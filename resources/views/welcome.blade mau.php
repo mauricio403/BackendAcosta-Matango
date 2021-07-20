8e1da16bd5408983281feb177cd6755dd7a395f3
mauricio403 <mauriciomatango_@hotmail.com>
2021-07-20 14:15:04 -0500

    Time of antojar

---
+++ resources/views/welcome.blade mau.php
@@ -0,0 +1,6 @@
+<h1>Es hora de antojar xdddd</h1>
+
+<h2>Que es poner F en el chat??</h2>
+<p>El origen de esta respuesta viene de una de las ediciones de la saga del videojuego 'Call of Duty', en concreto 'Call of Duty: Advanced Warfare'. En la historia que vertebra la ficción interactiva y sin destripar la trama, un personaje fallece y en su entierro el jugador tiene la opción de «presentar sus respuestos» al muerto si presiona la letra F en el teclado (aunque en la consola este recurso se activa pulsando la X o bien el cuadrado.
+
+El éxito de esta abreviatura de lenguaje digital, que aparece a menudo en los comentarios de los 'youtubers' y en los chats de los adolescentes, reside también en su simplicidad, que permite expresar rápida adhesión y comentar de manera rápida y cómplice a ese otro al que se contesta</p></P>
