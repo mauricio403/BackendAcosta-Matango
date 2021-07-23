8153b7bf55e76b2295694bae0c786c5da9c54224
mauricio403 <mauriciomatango_@hotmail.com>
2021-07-23 13:55:28 -0500

    Rutas creadas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 <h1>Acosta</h1>
-=======
+
 <h1>Mauricio Matango
     
 </h1>
->>>>>>> 1cba4471ed8c7d0f84a943f81fcddc7e05c92e1d
+
 
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('projects',function (){
+    return 'Obtenido 202';
+ });
+
+Route::post('projects',function (){
+    return "Creado 202";
+ });
+Route::put('projects',function (){
+    return 'Actualizado 202';
+ });
+Route::delete('projects',function (){
+    return 'Eliminado 202';
+ });
+
