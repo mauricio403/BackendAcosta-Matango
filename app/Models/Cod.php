c5c5a781a0f7d55f065f6c663265c9834f2871b5
mauricio403 <mauriciomatango_@hotmail.com>
2021-07-27 14:49:54 -0500

    xd

--- app/Models/Cod.php
+++ app/Models/Cod.php
@@ -5,13 +5,14 @@ namespace App\Models;
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Cod extends Model
 {
     // use HasFactory;
 
     protected $table = 'cods';
 
-    protected $attributes = [
+    protected $fillable = [
         'code',
         'description',
         'date',
@@ -19,11 +20,36 @@ class Cod extends Model
         'tittle',
     ];
 
-    public function cod(){
-        return $this ->hasOne(Cod::class);
+    public function cod()
+    {
+        return $this->hasOne(Cod::class);
     }
 
-
-
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'is_aprovved'=> 'boolean',
+        'date' => 'datetime: Y-m-d'
+   ];
 }
 
+// function authors()
+// {
+//     return $this->hasMany(Author::class);
+// }
+
+// function authors(){
+//     return $this->belongsToMany(Author::class);
+// }
+// function projects(){
+//     return $this->belongsToMany(Project::class);
+// }
+
+// ->withTimestamps();
+
+// function setPasswordAttribute($value){
+//     $this->attributes['password'] = Hash::make($value); 
+// }
+// function setCodeAttribute($value)
+// {
+//     $this->attributes['code'] = strtoupper($value);
+// }
