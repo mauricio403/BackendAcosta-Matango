10d0f1cfa9045a1ac502329b7e6cb1b43605c181
mauricio403 <mauriciomatango_@hotmail.com>
2021-07-27 13:39:55 -0500

    tabla cods

--- app/Models/Cod.php
+++ app/Models/Cod.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cod extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+
+    protected $table = 'cods';
+
+    protected $attributes = [
+        'code',
+        'description',
+        'date',
+        'is_aprovved',
+        'tittle',
+    ];
+
+    public function cod(){
+        return $this ->hasOne(Cod::class);
+    }
+
+
+
 }
+
