0b2d36d781741ea209c6ac9941b6152a8487e2d3
TammyPaola <tpc.acosta@yavirac.edu.ec>
2021-07-27 13:40:55 -0500

    relacion

--- app/Models/Rose.php
+++ app/Models/Rose.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Rose extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    
+    protected $table ='roses';
+
+
+    protected $attributes =['code',
+'descripcion',
+'date',
+'title',
+'is_approved',
+   ];
+
+
+   /*funtion project(){
+       return $this-> belondTo (relaled:cod::class);
+   }*/
+
+    public function rose(){
+    return$this->hasOne(related:Rose::class);
+
+        }
+      { 
+    }
 }
