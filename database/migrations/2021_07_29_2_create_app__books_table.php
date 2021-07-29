69ce77457e2fd5da211dc27f38c296ff98ef2743
mauricio403 <mauriciomatango_@hotmail.com>
2021-07-29 16:52:30 -0500

    subida a esquemas finished pao

---
+++ database/migrations/2021_07_29_2_create_app__books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(env('DB_CONNECTION_APP'))->create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(env('DB_CONNECTION_APP'))->dropIfExists('books');
+    }
+}
